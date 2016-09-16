f2c00c2243eb511d17795b3a8269862f719418ff
thefreshduke <scottyshaw1@gmail.com>
2016-09-15 22:36:36 -0500

    Created widget with screams

---
+++ MuhammedTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MuhammedTest
+//
+//  Created by Scotty Shaw on 9/15/16.
+//  Copyright © 2016 ___sks6___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.blueColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
